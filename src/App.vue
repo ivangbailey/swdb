aef5572f53aa2d0156fde6a8f668a256096f26a2
Ivan Bailey <ivangbailey@gmail.com>
2021-09-05 11:21:29 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div id="logo-container">
+    <img alt="Star Wars logo" src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg">
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
@@ -15,13 +17,25 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+body, html
+  background-color: black
+
+h1, h2, h3
+  color: #ffe81f
+
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+#logo-container
+  max-width: 8em
+  margin: 1em auto
+
+img
+  max-width: 100%
 </style>
